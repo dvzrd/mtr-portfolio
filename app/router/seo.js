60a65f9fef80d7a00961a3a7c43328df31d5f97a
Damir <damir@withpulp.com>
2015-08-16 05:07:44 -0400

    updates to seo, added social module

---
+++ app/router/seo.js
@@ -0,0 +1,12 @@
+SEO = new FlowRouterSEO();
+
+SEO.setDefaults({
+  title: 'Meteor Portfolio | A Portfolio App for Artists and Photographers',
+  description: 'A portfolio app for artists or photographers built with Meteor using Semantic-UI and Orion CMS',
+  meta: {
+    'property="og:type"': 'website',
+    'property="og:site_name"': 'Meteor Portfolio',
+    'name="twitter:card"': 'summary',
+    'name="twitter:site"': '@TwitterUsername'
+  }
+});
