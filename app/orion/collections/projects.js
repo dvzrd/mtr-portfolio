b32db046ab4994903c96f70885390a1eb497cfed
Damir <damir@withpulp.com>
2015-08-21 02:57:31 -0400

    added description to project schema

--- app/orion/collections/projects.js
+++ app/orion/collections/projects.js
@@ -1,47 +1,50 @@
 Projects = new orion.collection('projects', {
-  singularName: 'project',
-  pluralName: 'Projects',
-  title: 'Projects',
-  link: {
-    title: 'Projects'
-  },
-  tabular: {
-    columns: [
-		  {
-			  data: 'title',
-			  title: 'Title'
-		  },
-      {
-        data: 'thumb',
-        title: 'Thumbnail'
-      },
-      orion.attributeColumn('createdAt', 'createdAt', 'Date')
-    ]
-  }
+    singularName: 'project',
+    pluralName: 'Projects',
+    title: 'Projects',
+    link: {
+        title: 'Projects'
+    },
+    tabular: {
+        columns: [
+            {
+                data: 'title',
+                title: 'Title'
+            },
+            {
+                data: 'description',
+                title: 'Description'
+            },
+            orion.attributeColumn('createdAt', 'createdAt', 'Date')
+        ]
+    }
 });
 
 Projects.attachSchema(new SimpleSchema({
-  title: {
-    type: String,
-    label: 'Title'
-  },
-	thumb: {
-	  type: String,
-		label: 'Thumbnail'
-  },
-  album: {
-    type: [Object],
-    minCount: 1,
-    maxCount: 50
-  },
-  'album.$.title': {
-    type: String,
-    label: 'Image Title'
-  },
-  'album.$.image': {
-    type: String,
-    label: 'Image Path'
-  },
-  createdBy: orion.attribute('createdBy'),
-  createdAt: orion.attribute('createdAt')
+    title: {
+        type: String,
+        label: 'Title'
+    },
+    thumb: {
+        type: String,
+        label: 'Thumbnail'
+    },
+    description: orion.attribute('froala', {
+        label: 'Description'
+    })
+    album: {
+        type: [Object],
+        minCount: 1,
+        maxCount: 50
+    },
+    'album.$.title': {
+        type: String,
+        label: 'Image Title'
+    },
+    'album.$.image': {
+        type: String,
+        label: 'Image Path'
+    },
+    createdBy: orion.attribute('createdBy'),
+    createdAt: orion.attribute('createdAt')
 }));
