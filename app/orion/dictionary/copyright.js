a1cef99a4f19ed235754d33a9099356e30ce7413
Damir <damir@withpulp.com>
2015-08-21 07:11:12 -0400

    created dict for copyright module

---
+++ app/orion/dictionary/copyright.js
@@ -0,0 +1,19 @@
+orion.dictionary.addDefinition('year', 'copyright', {
+    type: String,
+    label: 'Copyright Year'
+});
+
+orion.dictionary.addDefinition('creator', 'copyright', {
+    type: String,
+    label: 'Creator'
+});
+
+orion.dictionary.addDefinition('url', 'copyright', {
+    type: String,
+    label: 'Creator\'s URL'
+});
+
+orion.dictionary.addDefinition('url', 'message', {
+    type: String,
+    label: 'Copyright Message'
+});
