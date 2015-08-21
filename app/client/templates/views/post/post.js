29cdb2f115838e9804255c429d03b8cc0aeaa66e
Damir <damir@withpulp.com>
2015-08-21 15:42:10 -0400

    created post view

---
+++ app/client/templates/views/post/post.js
@@ -0,0 +1,15 @@
+Template.post.onCreated(function() {
+    var self = this;
+    self.autorun(function() {
+        var projectId = FlowRouter.getParam('postId');
+        self.subscribe('post', postId);
+    });
+});
+
+Template.post.helpers({
+    post: function() {
+        var postId = FlowRouter.getParam('postId');
+        var post = Posts.findOne({_id: postId}) || {};
+        return post;
+    }
+});
