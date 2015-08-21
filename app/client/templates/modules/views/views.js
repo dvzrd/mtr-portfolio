2e587cc6b09efc59cb6d3727060fd473abf3f262
Damir <damir@withpulp.com>
2015-08-21 06:30:45 -0400

    moved menu to dict - removed views collection and module

--- app/client/templates/modules/views/views.js
+++
@@ -1,9 +0,0 @@
-Template.views.onRendered(function() {
-	Meteor.subscribe('views');
-});
-
-Template.views.helpers({
-  views: function () {
-    return Views.find({}, {limit: 4, sort: {order: 1}});
-  }
-});
---
+++ app/orion/dictionary/menu.js
@@ -0,0 +1,24 @@
+orion.dictionary.addDefinition('root', 'menu', {
+    type: String,
+    label: 'Root Label'
+});
+
+orion.dictionary.addDefinition('about', 'menu', {
+    type: String,
+    label: 'About Label'
+});
+
+orion.dictionary.addDefinition('blog', 'menu', {
+    type: String,
+    label: 'Blog Label'
+});
+
+orion.dictionary.addDefinition('contact', 'menu', {
+    type: String,
+    label: 'Contact Label'
+});
+
+orion.dictionary.addDefinition('admin', 'menu', {
+    type: String,
+    label: 'Admin Label'
+});
