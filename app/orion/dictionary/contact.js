f8b44216f79701a6ed3b81911fed321c9638b771
Damir <damir@withpulp.com>
2015-08-21 14:36:36 -0400

    styles for contact view

--- app/orion/dictionary/contact.js
+++ app/orion/dictionary/contact.js
@@ -3,6 +3,21 @@ orion.dictionary.addDefinition('title', 'contact', {
     label: 'Title'
 });
 
+orion.dictionary.addDefinition('icon', 'contact', {
+    type: String,
+    label: 'Icon'
+});
+
+orion.dictionary.addDefinition('email', 'contact', {
+    type: String,
+    label: 'Email'
+});
+
+orion.dictionary.addDefinition('message', 'contact', {
+    type: String,
+    label: 'Message'
+});
+
 orion.dictionary.addDefinition('image', 'contact', {
     type: String,
     label: 'Title'
