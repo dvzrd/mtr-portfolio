55ca40302b462911ef4a832e748d11fae547b424
Damir <damir@withpulp.com>
2015-08-21 08:32:05 -0400

    updates to header and about

---
+++ app/orion/dictionary/about.js
@@ -0,0 +1,8 @@
+orion.dictionary.addDefinition('image', 'about', {
+    type: String,
+    label: 'Image Path'
+});
+
+orion.dictionary.addDefinition('content', 'about',
+    orion.attribute('froala')
+);
