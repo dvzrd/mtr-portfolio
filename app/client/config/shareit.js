0e4dac7351587e43f0c7141d53fe432e0b1ece3c
Damir <damir@withpulp.com>
2015-08-24 15:45:44 -0400

    replaced social media buttons with shareit module

---
+++ app/client/config/shareit.js
@@ -0,0 +1,15 @@
+ShareIt.configure({
+    sites: {
+        'facebook': {
+            'appId': null
+        },
+        'twitter': {},
+        'googleplus': {},
+        'pinterest': {}
+    },
+    classes: 'ui icon button',
+    iconOnly: true,
+    applyColors: false,
+    faSize: '',
+    faClass: ''
+});
