a2ef90a3a270b0807786f0576d25f930548a15cb
Damir <damir@withpulp.com>
2015-08-26 19:14:50 -0400

    next/prev project routes wip

--- app/client/templates/views/project/project.js
+++ app/client/templates/views/project/project.js
@@ -3,8 +3,8 @@ Template.project.onCreated(function () {
     self.autorun(function () {
         var projectId = FlowRouter.getParam('projectId');
         self.subscribe('project', projectId);
-        self.subscribe('projectPrev', projectId);
-        self.subscribe('projectNext', projectId);
+        //self.subscribe('projectPrev', projectId);
+        //self.subscribe('projectNext', projectId);
     });
 
     $('.ui.sticky').sticky();
@@ -15,5 +15,16 @@ Template.project.helpers({
         var projectId = FlowRouter.getParam('projectId');
         var project = Projects.findOne({_id: projectId}) || {};
         return project;
+    }
+});
+
+Template.project.events({
+    'click .ui.prev.button': function() {
+        var projectId = FlowRouter.getParam('projectId');
+        FlowRouter.path('/work/' + (this.id + 1));
+        console.log(FlowRouter.path('/work/' + (projectId)));
+    },
+    'click .ui.next.button': function() {
+        FlowRouter.path('/work/:id', {_id: this.id + 1});
     }
 });
