9e0245f44349b74d8d166dc9cdf8318b31cfbc2d
Damir <damir@withpulp.com>
2015-09-07 11:25:34 -0400

    created author dict

---
+++ app/orion/dictionary/author.js
@@ -0,0 +1,49 @@
+orion.dictionary.addDefinition('name', 'author', {
+    type: String,
+    label: 'Name'
+});
+
+orion.dictionary.addDefinition('email', 'author', {
+    type: String,
+    label: 'Email'
+});
+
+orion.dictionary.addDefinition('image', 'author', {
+    type: String,
+    label: 'Image'
+});
+
+orion.dictionary.addDefinition('website', 'author', {
+    type: String,
+    label: 'Website URL'
+});
+
+orion.dictionary.addDefinition('messagePrimary', 'author', {
+    type: String,
+    label: 'Primary Message',
+    optional: true
+});
+
+orion.dictionary.addDefinition('urlPrimary', 'author', {
+    type: String,
+    label: 'Primary URL',
+    optional: true
+});
+
+orion.dictionary.addDefinition('messageBreak', 'author', {
+    type: String,
+    label: 'Message Break',
+    optional: true
+});
+
+orion.dictionary.addDefinition('messageSecondary', 'author', {
+    type: String,
+    label: 'Secondary Message',
+    optional: true
+});
+
+orion.dictionary.addDefinition('urlSecondary', 'author', {
+    type: String,
+    label: 'Secondary URL',
+    optional: true
+});
